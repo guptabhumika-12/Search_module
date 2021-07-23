3269fedc5f5da8af3c32f0e4ccbc357ffe50b5d3
guptabhumika-12 <bhumika12052001@gmail.com>
2021-07-23 14:09:30 +0530

    new pages added

--- prototype/src/components/Header.js
+++ prototype/src/components/Header.js
@@ -1,7 +1,8 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import "../StyleSheet/Header.css";
 import FlipkartLogo from "./FlipkartLogo";
+
 const Header = () => {
   const [searchString, setsearchString] = useState("");
   const handleChange = useCallback(
@@ -26,6 +27,22 @@ const Header = () => {
       ? alert(searchString)
       : alert("please enter a product to search");
   };
+
+  useEffect(() => {
+    const url = "http://localhost:8000/product";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="header">
